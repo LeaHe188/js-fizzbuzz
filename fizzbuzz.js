6b65738fb31db38e2789d26c552447d8c446a7f3
Leah He <Leah.He@softwire.com>
2024-07-02 10:50:34 +0100

    task 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,43 @@
 // This is our main function
 function fizzbuzz() {
-    for (let i = 1; i <= 100; i++) {
+    let result = []
+    let index = -1
+    for (let i = 1; i <= 15; i++) {
         if (i % 5 === 0 && i % 3 === 0 ) {
-            console.log(`FizzBuzz`)
+            result.push('Fizz')
+            result.push('Buzz')
         } else if (i % 3 === 0 ) {
-            console.log(`Fizz`)
+            result.push('Fizz')
         } else if (i % 5 === 0 ) {
-            console.log(`Buzz`)
+            result.push('Buzz')
         }
-        else {
-            console.log(`${i}`)
+
+        if (i % 7 === 0 ) {
+            result.push('Bang')
         }
+
+        if (i % 11 === 0 ) {
+            result = ['Bong']
+        }
+
+        if (i % 13 === 0 ) {
+            index = result.indexOf('Buzz')
+            if (index === -1) {
+                result.unshift('Fezz')
+            }
+            else {
+                result.splice(index, 0, 'Fezz')
+            }
+        }
+        if (i % 17 === 0) {
+            result.reverse()
+        }
+        if (result.length === 0) {
+            console.log(i)
+        } else {
+            console.log(result.join(''))
+        }
+        result = []
     }
 
 }
